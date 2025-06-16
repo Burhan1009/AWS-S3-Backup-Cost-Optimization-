d796b49d5277dd1f0be42b71c582e1c7d02545c2
Burhan <techdeployers.com@gmail.com>
2025-06-16 10:30:36 +0530

    Update output.tf

--- Terraform/output.tf
+++ Terraform/output.tf
@@ -7,3 +7,12 @@ output "region" {
   description = "The AWS region used"
   value       = var.region
 }
+
+output "sns_notification_email" {
+  description = "SNS email address to receive S3 upload notifications"
+  value       = var.notification_email
+}
+output "sns_topic_arn" {
+  description = "ARN of the SNS topic for S3 notifications"
+  value       = aws_sns_topic.s3_notifications.arn
+}
